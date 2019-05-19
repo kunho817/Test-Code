ec21336fd06a1cd094014e1f0658e674edf8dd38
BVCLiper <kunho817@naver.com>
2019-05-19 20:05:00 +0900

    MainWindow 업데이트

--- Profile/MainWindow.xaml.vb
+++ Profile/MainWindow.xaml.vb
@@ -1,3 +1,24 @@
 ﻿Class MainWindow
+    Dim fileR As String
+    Dim thefile As String
+    Dim results As String
+    Dim person As System.IO.FileStream
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        thefile = String.Format("c:\{0}.txt", Name.Text)
+        results = Dir$(thefile)
+        If results = "" Then
+            person = System.IO.File.Create(String.Format("c:\{0}.txt", Name.Text))
+            MsgBox("New Information is added")
+        Else
+            fileR = My.Computer.FileSystem.ReadAllText(String.Format("c:\{0}.txt", InputName.Text),
+            System.Text.Encoding.UTF8)
+            Text.Text = fileR
+        End If
+    End Sub
+
+    Private Sub add(sender As Object, e As RoutedEventArgs)
+
+    End Sub
 
 End Class
