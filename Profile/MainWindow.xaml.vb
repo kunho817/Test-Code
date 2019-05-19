291813a050e9bd7718c88591b202951011d3efd0
BVCLiper <kunho817@naver.com>
2019-05-19 20:31:18 +0900

    대거 수정

--- Profile/MainWindow.xaml.vb
+++ Profile/MainWindow.xaml.vb
@@ -5,20 +5,39 @@
     Dim person As System.IO.FileStream
 
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        thefile = String.Format("c:\{0}.txt", Name.Text)
+        thefile = String.Format("c:\Profile\{0}.txt", Name.Text)
         results = Dir$(thefile)
         If results = "" Then
-            person = System.IO.File.Create(String.Format("c:\{0}.txt", Name.Text))
-            MsgBox("New Information is added")
+            MsgBox("이 이름을 가진 사람의 정보가 존재하지 않습니다.")
+            Canvas.IsEnabled = True
+            Name.Text = InputName.Text
         Else
-            fileR = My.Computer.FileSystem.ReadAllText(String.Format("c:\{0}.txt", InputName.Text),
+            fileR = My.Computer.FileSystem.ReadAllText(String.Format("c:\Profile\{0}.txt", InputName.Text),
             System.Text.Encoding.UTF8)
             Text.Text = fileR
         End If
     End Sub
 
-    Private Sub add(sender As Object, e As RoutedEventArgs)
-
+    Private Sub Add(sender As Object, e As RoutedEventArgs)
+        For i As Integer = 0 To i < Name.Text.Length
+            Name.
+        Next
+        thefile = String.Format("c:\Profile\{0}.txt", Name.Text)
+        results = Dir$(thefile)
+        If results = "" Then
+            person = System.IO.File.Create(String.Format("c:\Profile\{0}.txt", Name.Text))
+            MsgBox("New Information is added")
+            Canvas.IsEnabled = False
+        End If
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter(String.Format("c:\Profile\{0}.txt", Name.Text), False)
+        file.WriteLine(Name.Text)
+        file.WriteLine(Age.Text)
+        file.WriteLine(Job.Text)
+        file.WriteLine(Birth.Text)
+        file.WriteLine(Country.Text)
+        file.WriteLine(Location.Text)
+        file.Close()
     End Sub
 
 End Class
