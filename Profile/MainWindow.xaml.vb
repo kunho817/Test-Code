c08badb659a8a0cbbb3077db4d80fd28a3828383
BVCLiper <kunho817@naver.com>
2019-05-24 18:27:15 +0900

    온갖 짓 다했습니다. 추가 내용 -combobox를 텍스트 입력형으로 전환해서 사람 이름을 칠때 그 이름과 동명 이인인 사람이 다 나오도록 제작 -사람 파일을 추가하면 Com.txt라는 파일에 그 파일 이름을 저장, combobox로 불러올때 사용합니다. -Save 버튼 추가 및 동적 할당으로 인한 프로세스 충돌 해결 그 외 기타 등등

--- Profile/MainWindow.xaml.vb
+++ Profile/MainWindow.xaml.vb
@@ -2,42 +2,78 @@
     Dim fileR As String
     Dim thefile As String
     Dim results As String
-    Dim person As System.IO.FileStream
 
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        thefile = String.Format("c:\Profile\{0}.txt", Name.Text)
+        thefile = String.Format("c:\Profile\{0}.txt", InputCombo.Text)
         results = Dir$(thefile)
         If results = "" Then
             MsgBox("이 이름을 가진 사람의 정보가 존재하지 않습니다.")
             Canvas.IsEnabled = True
-            Name.Text = InputName.Text
+            Nam.Text = InputCombo.Text
         Else
-            fileR = My.Computer.FileSystem.ReadAllText(String.Format("c:\Profile\{0}.txt", InputName.Text),
-            System.Text.Encoding.UTF8)
+            fileR = My.Computer.FileSystem.ReadAllText(String.Format("c:\Profile\{0}.txt", InputCombo.Text), System.Text.Encoding.UTF8)
             Text.Text = fileR
         End If
     End Sub
 
     Private Sub Add(sender As Object, e As RoutedEventArgs)
-        For i As Integer = 0 To i < Name.Text.Length
-            Name.
-        Next
-        thefile = String.Format("c:\Profile\{0}.txt", Name.Text)
+        Dim person As System.IO.FileStream
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter(("c:\Profile\Com\Com.txt"), False)
+
+        thefile = String.Format("c:\Profile\{0}_{1}_{2}_{3}.txt", InputCombo.Text, Age.Text, Birth.Text, Country.Text)
         results = Dir$(thefile)
         If results = "" Then
-            person = System.IO.File.Create(String.Format("c:\Profile\{0}.txt", Name.Text))
+            person = System.IO.File.Create(String.Format("c:\Profile\{0}_{1}_{2}_{3}.txt", InputCombo.Text, Age.Text, Birth.Text, Country.Text))
+            InputCombo.Items.Add(thefile)
             MsgBox("New Information is added")
-            Canvas.IsEnabled = False
+            file.WriteLine(String.Format("{0}_{1}_{2}_{3}", InputCombo.Text, Age.Text, Birth.Text, Country.Text))
+            file.Close()
+            person.Close()
         End If
-        Dim file As System.IO.StreamWriter
-        file = My.Computer.FileSystem.OpenTextFileWriter(String.Format("c:\Profile\{0}.txt", Name.Text), False)
-        file.WriteLine(Name.Text)
-        file.WriteLine(Age.Text)
-        file.WriteLine(Job.Text)
-        file.WriteLine(Birth.Text)
-        file.WriteLine(Country.Text)
-        file.WriteLine(Location.Text)
-        file.Close()
     End Sub
 
+    Private Sub Save(sender As Object, e As RoutedEventArgs)
+        If (Canvas.IsEnabled) Then
+            Dim file As System.IO.StreamWriter
+            file = My.Computer.FileSystem.OpenTextFileWriter(String.Format("c:\Profile\{0}_{1}_{2}_{3}.txt", InputCombo.Text, Age.Text, Birth.Text, Country.Text), False)
+            file.WriteLine(Nam.Text)
+            file.WriteLine(Age.Text)
+            file.WriteLine(Job.Text)
+            file.WriteLine(Birth.Text)
+            file.WriteLine(Country.Text)
+            file.WriteLine(Location.Text)
+            file.Close()
+            Canvas.IsEnabled = False
+            file.Close()
+        End If
+    End Sub
+
+    Private Sub Activates(sender As Object, e As EventArgs)
+        Dim filess As String
+        Dim i As Integer
+        Dim str As String
+        Dim News As System.IO.StreamReader
+        Dim fileo As System.IO.FileStream
+
+        filess = "c:\Profile\Com\Com.txt"
+
+        results = Dir$(filess)
+        If results = "" Then
+            fileo = System.IO.File.Create("c:\Profile\Com\Com.txt")
+            fileo.Close()
+        Else
+            News = My.Computer.FileSystem.OpenTextFileReader("c:\Profile\Com\Com.txt", System.Text.Encoding.UTF8)
+            str = My.Computer.FileSystem.ReadAllText("c:\Profile\Com\Com.txt", System.Text.Encoding.UTF8)
+
+
+            For i = 0 To i < str.Length
+                If (StrComp("\0", str.Chars(i))) Then
+                    InputCombo.Items.Add(News.ReadLine)
+                End If
+            Next
+        End If
+
+
+    End Sub
 End Class
